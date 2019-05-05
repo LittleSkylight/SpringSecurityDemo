698e6208803918a3eb7629dad9f6c6d19032d22d
Skylight <907310977@qq.com>
2019-05-06 04:38:00 +0800

    <添加>添加角色：ROLE_GOD

--- src/main/java/com/springsecurity/demo/controller/UserController.java
+++ src/main/java/com/springsecurity/demo/controller/UserController.java
@@ -3,6 +3,7 @@ package com.springsecurity.demo.controller;
 import com.springsecurity.demo.model.SysUser;
 import com.springsecurity.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +16,11 @@ public class UserController {
     @RequestMapping("/doRegister")
     public boolean doRegister(SysUser user) {
         return userService.addUser(user);
+    }
+
+    @RequestMapping("/god")
+    @PreAuthorize("hasRole('ROLE_GOD')")
+    public String hiGod(){
+        return "Hello God！";
     }
 }
